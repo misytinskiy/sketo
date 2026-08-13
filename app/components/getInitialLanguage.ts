dc667bebdb98c4c87771e8d4e824f30e1d337460
traumerch-dev <tech@traumerch.com>
2026-08-13 14:48:45 +0400

    fix deploy issue

--- app/components/getInitialLanguage.ts
+++ app/components/getInitialLanguage.ts
@@ -3,7 +3,7 @@ import { LANGUAGE_COOKIE_KEY, normalizeLanguage } from "./language";
 
 export async function getInitialLanguage(): Promise<"ru" | "en"> {
   const cookieStore = await cookies();
-  const language = cookieStore.get(LANGUAGE_COOKIE_KEY)?.value;
+  const language = cookieStore.get(LANGUAGE_COOKIE_KEY)?.value ?? null;
 
   return normalizeLanguage(language);
 }
